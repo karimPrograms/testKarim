7f59c71759c8cf8d1160f9c6e5706101cb6b8e05
karimPrograms <78750964+karimPrograms@users.noreply.github.com>
2022-04-12 00:53:06 +0100

    Update MainPage.php

--- Projet_These/Projet_These/site/MainPage.php
+++ Projet_These/Projet_These/site/MainPage.php
@@ -1,3 +1,14 @@
+<?php 
+include '../login/config.php';
+
+session_start();
+
+if (!isset($_SESSION['username'])) {
+    header("Location: index.php");
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -14,58 +25,115 @@
         </ul>
       </div>
     </nav>
+    
+    
     <div class="myWebSite">
       <div class="formulaire">
-        <div class="titre"><h1>Formulaire</h1></div>
+        <div class="titre"><h2>Formulaire</h2></div>
+
         <form action="#" method="post">
+
           <div class="radioInput">
-            <label for="type"> Type de produit:</label>
-            Médicament
-            <input type="radio" id="type" name="typeP" />
-            MatérialPharm
-            <input type="radio" name="typeP" id="type" />
+            <label for="type" class='labelname'> Type de Produit:</label>
+
+            <label class="radiochoice"> Médicament 
+              <input type="radio" id="type" name="typeP" value="Medicament" checked="checked"/>
+              <span class="checkmark"></span>
+            </label>
+
+            <label class="radiochoice"> Matérial Pharmaceutique
+              <input type="radio" name="typeP" id="type" value="Material Pharmaceutique"/>
+              <span class="checkmark"></span>
+            </label>
           </div>
+
           <div class="dataInput">
             <label for="NomP">Nom de Produit:</label>
             <input
               type="text"
               name="NomP"
-              id="NomP"
+              id="Nom"
               placeholder="Tapez le nom"
               required
             />
           </div>
+
           <div class="dataInput">
             <label for="mili"> Millgramme :</label>
             <input
-              type="text"
-              nom="Milligramme"
+              type="number"
+              name="Milligramme"
               id="mili"
-              placeholder="Tapez les milligramme"
+              placeholder="Tapez les milligrammes"
+              required  
             />
           </div>
+
           <div class="dataInput">
             <label for="Prix"> Prix:</label>
             <input
               type="number"
-              nom="Prix"
+              name="Prix"
               id="Prix"
               placeholder="Tapez le prix"
               required     
             />
           </div>
+
           <div class="radioInput">
-            <label for="Dispo">Disponible:</label>
-            <span>oui </span> <input type="radio" name="Dispo" id="Dispo" />
-            <span>non</span>
-            <input type="radio" name="Dispo" id="Dispo" />
+            <label for="Dispo" class='labelname'>Disponible:</label>
+
+            <label class="radiochoice"> Oui 
+              <input type="radio" id="type" name="Dispo" checked="checked" value="Oui"/>
+              <span class="checkmark"></span>
+            </label>
+
+            <label class="radiochoice"> Non
+              <input type="radio" id="type" name="Dispo" value="Non"/>
+              <span class="checkmark"></span>
+            </label>
           </div>
+
           <div class="dataInput">
-            <input type="submit" value="Registrer" id="myBtn" />
+            <input type="submit" value="Ajouter" id="myBtn" name="Submit" />
           </div>
+
         </form>
       </div>
     </div>
-    <script src="index.js"></script>
+
+    <div class="MedicaleTable">
+        <div class="Mytable">
+          <table class="Table">
+            <thead>
+              <tr>
+                <th>Type</th>
+                <th>Nom</th>
+                <th>Milligramme</th>
+                <th>prix</th>
+                <th>Disponible</th>
+              </tr>
+            </thead>
+
+            <?php
+            $sql = "SELECT Type, Nom, Miligramme, Prix, Disponible FROM medicament";
+            $result = $mysqli->query($sql);
+
+            if($result->num_rows > 0){
+              while($row = $result->fetch_assoc()){
+                echo "<tr><td>". $row["Type"]. "</td><td>". $row["Nom"].
+                "</td><td>". $row["Miligramme"]."</td><td>". $row["Prix"].
+                "</td><td>". $row["Disponible"]."</td></tr>";
+              }
+              echo "</table>";
+            }else{
+              echo "0 Results";
+            }
+            ?>
+          </table>
+        </div>
+      </div>
+
+    <script src="./index.js"></script>
   </body>
 </html>
