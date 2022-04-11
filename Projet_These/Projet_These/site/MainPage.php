bc578d16c84d3c6312cde8ac2c21ae08098736c7
Rachid-Mek <78279178+Rachid-Mek@users.noreply.github.com>
2022-04-11 13:59:40 +0100

    Update MainPage.php

--- Projet_These/Projet_These/site/MainPage.php
+++ Projet_These/Projet_These/site/MainPage.php
@@ -1,22 +1,70 @@
-<?php 
-
-session_start();
-
-if (!isset($_SESSION['username'])) {
-    header("Location: index.php");
-}
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="mystyle.css" />
     <title>Welcome</title>
-</head>
-<body>
-    <?php echo "<h1>Welcome " . $_SESSION['username'] . "</h1>"; ?>
-    <a href="../login/logout.php">Logout</a>
-</body>
+  </head>
+  <body>
+    <nav class="navBar">
+      <div class="container">
+        <ul class="flex-bar">
+          <li><a href="../login/logout.php">Logout</a></li>
+        </ul>
+      </div>
+    </nav>
+    <div class="myWebSite">
+      <div class="formulaire">
+        <div class="titre"><h1>Formulaire</h1></div>
+        <form action="#" method="post">
+          <div class="radioInput">
+            <label for="type"> Type de produit:</label>
+            Médicament
+            <input type="radio" id="type" name="typeP" />
+            Matérial Pharmaceutiques
+            <input type="radio" name="typeP" id="type" />
+          </div>
+          <div class="dataInput">
+            <label for="NomP">Nom de Produit:</label>
+            <input
+              type="text"
+              name="NomP"
+              id="NomP"
+              placeholder="Tapez le nom"
+              required
+            />
+          </div>
+          <div class="dataInput">
+            <label for="mili"> Millgramme :</label>
+            <input
+              type="text"
+              nom="Milligramme"
+              id="mili"
+              placeholder="Tapez les milligramme"
+            />
+          </div>
+          <div class="dataInput">
+            <label for="Prix"> Prix:</label>
+            <input
+              type="number"
+              nom="Prix"
+              id="Prix"
+              placeholder="Tapez le prix"
+              required     
+            />
+          </div>
+          <div class="radioInput">
+            <label for="Dispo">Disponible:</label>
+            <span>oui </span> <input type="radio" name="Dispo" id="Dispo" />
+            <span>non</span>
+            <input type="radio" name="Dispo" id="Dispo" />
+          </div>
+          <div class="dataInput">
+            <input type="submit" value="Registrer" id="myBtn" />
+          </div>
+        </form>
+      </div>
+    </div>
+  </body>
 </html>
