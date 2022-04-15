c0fdde4b7caab6da9a62aab540fa0cdb3bed50cc
karimPrograms <78750964+karimPrograms@users.noreply.github.com>
2022-04-15 18:06:11 +0100

    Add files via upload

---
+++ Projet_These/Projet_These/site/updatecode.php
@@ -0,0 +1,34 @@
+<?php
+
+include '../login/config.php'; 
+session_start();
+
+if (!isset($_SESSION['username'])) {
+    header("Location: ../login/login.php");
+}
+
+if(isset($_POST['updatedata'])){
+
+    $id = $_POST['update_id'];
+    $type = $_POST['typeD'];
+    $nom = $_POST['NomD'];
+    $milli = $_POST['MilligrammeD'];
+    $prix = $_POST['PrixD'];
+    $dispo = $_POST['DispoD'];
+
+        $query = "UPDATE medicament SET Type='$type', Nom='$nom', Miligramme='$milli', Prix=' $prix', Disponible=' $dispo' WHERE Med_Id='$id'  ";
+        $query_run = mysqli_query($mysqli, $query);
+
+        if($query_run)
+        {
+            echo '<script> alert("Data Updated"); </script>';
+            header("Location:MainPage.php");
+        }
+        else
+        {
+            echo '<script> alert("Data Not Updated"); </script>';
+        }
+
+}
+
+?>
