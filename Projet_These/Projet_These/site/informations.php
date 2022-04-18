fd5ad9f7e7f3e252c69c360efdc21c4fac218a99
karimPrograms <78750964+karimPrograms@users.noreply.github.com>
2022-04-18 21:21:46 +0100

    Update informations.php

--- Projet_These/Projet_These/site/informations.php
+++ Projet_These/Projet_These/site/informations.php
@@ -1,3 +1,31 @@
 <?php
-echo'HI';
-?>
+
+include '../login/config.php'; 
+session_start();
+
+if (!isset($_SESSION['username'])) {
+    header("Location: ../login/login.php");
+}
+
+if(isset($_POST['EditInfo'])){
+
+    $id = $_SESSION['username'];
+    $pharmName = $_POST['pharmName'];
+    $pharmNum1 = $_POST['pharmNum1'];
+    $pharmNum2 = $_POST['pharmNum2'];
+    $pharmAdr = $_POST['pharmAdr'];
+    $pharmEmail = $_POST['pharmEmail'];
+
+        $query = "UPDATE pharma_info SET Name='$pharmName', Contact_Number1	='$pharmNum1', Contact_Number2='$pharmNum2', Location='$pharmAdr', Email='$pharmEmail' WHERE Id='$id'";
+        $query_run = mysqli_query($mysqli, $query);
+
+        if($query_run)
+        {
+            header("Location:MainPage.php");
+        }
+        else
+        {
+            echo '<script> alert("Data Not Updated"); </script>';
+        }
+
+}
